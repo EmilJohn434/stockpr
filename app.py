0be4e6c1a63b50b6e140f17bb55165c87b9dd098
EmilJohn434 <emilabraham434@gmail.com>
2024-12-12 02:32:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,48 +9,15 @@ from prophet import Prophet
 from prophet.plot import plot_plotly
 import plotly.graph_objs as go
 import pandas as pd
-import requests
-from textblob import TextBlob
 
 TODAY = date.today().strftime("%Y-%m-%d")
-NEWS_API_KEY = '49f6efd7a04141d5ac76df46a930678d'  # Replace with your News API key
 start_date = None
-
 
 def load_data(ticker):
     if ticker:
         data = yf.download(ticker, start_date, TODAY)
         data.reset_index(inplace=True)
         return data
-
-
-def get_news(stock):
-    if NEWS_API_KEY:
-        company_name = yf.Ticker(stock).info['longName']
-        search_query = f'{stock} OR {company_name}'
-        url = f'https://newsapi.org/v2/everything?q={search_query}&apiKey={NEWS_API_KEY}&pageSize=5'
-        response = requests.get(url)
-        news_data = response.json()
-        return news_data
-
-
-def analyze_sentiment(text):
-    blob = TextBlob(text.lower())
-    sentiment_score = 0
-
-    for word in blob.words:
-        if word in positive_words:
-            sentiment_score += 1
-        elif word in negative_words:
-            sentiment_score -= 1
-
-    return sentiment_score
-
-
-# Custom word lists for positive and negative sentiment
-positive_words = ['good', 'excellent', 'positive', 'improve', 'success', 'up', 'gain', 'bullish', 'happy', 'prosper',
-                  'opportunity']
-negative_words = ['bad', 'poor', 'negative', 'decline', 'failure', 'down', 'loss', 'bearish', 'sad', 'danger', 'risk']
 
 st.title('Stock Market Predictor')
 
@@ -78,13 +45,11 @@ if choice == "Predict Single Stock":
         daily_data = data.resample('D').interpolate()
         daily_data['Close_rolling'] = daily_data['Close'].ewm(alpha=1 - smoothing_factor).mean()
 
-
         def plot_raw_data():
             fig = go.Figure()
             fig.add_trace(go.Scatter(x=daily_data.index, y=daily_data['Open'], name="Stock Open"))
             fig.add_trace(go.Scatter(x=daily_data.index, y=daily_data['Close'], name="Stock Close"))
-            fig.add_trace(
-                go.Scatter(x=daily_data.index, y=daily_data['Close_rolling'], name="Close (Exponential Smoothing)"))
+            fig.add_trace(go.Scatter(x=daily_data.index, y=daily_data['Close_rolling'], name="Close (Exponential Smoothing)"))
             fig.update_layout(
                 title_text='Stock History',
                 xaxis_rangeslider_visible=True,
@@ -93,29 +58,7 @@ if choice == "Predict Single Stock":
             )
             st.plotly_chart(fig)
 
-
         plot_raw_data()
-
-        news_data = get_news(selected_stock)
-
-        overall_sentiment_score = 0
-        if 'articles' in news_data and len(news_data['articles']) > 0:
-            for article in news_data['articles']:
-                # Check if the description has a minimum word count to consider it relevant
-                min_word_count = 10
-                if len(article['description'].split()) >= min_word_count:
-                    st.write(f"**URL:** {article['url']}")
-                    sentiment_score = analyze_sentiment(article['description'])
-                    weight = 10
-                    sentiment_score *= weight
-                    overall_sentiment_score += sentiment_score
-
-        if overall_sentiment_score > 0:
-            st.subheader("Overall Sentiment: Positive")
-        elif overall_sentiment_score < 0:
-            st.subheader("Overall Sentiment: Negative")
-        else:
-            st.subheader("Overall Sentiment: Neutral")
 
         df_train = daily_data[['Close_rolling']].reset_index().rename(columns={"Date": "ds", "Close_rolling": "y"})
 
@@ -174,7 +117,6 @@ elif choice == "Compare Stocks":
             data = pd.concat([data, stock_data], ignore_index=True)
         data_load_state.text('Loading data... done!')
 
-
         def plot_comparison():
             fig = go.Figure()
             for stock in selected_stocks:
@@ -188,7 +130,6 @@ elif choice == "Compare Stocks":
                 width=900
             )
             st.plotly_chart(fig)
-
 
         plot_comparison()
 
@@ -204,30 +145,9 @@ footer = """
 }
 </style>
 <div class="footer">
-    <p>Coded by Pranav,Emil and Mantra</p>
+    <p>Coded by Pranav, Emil, and Mantra</p>
     <p>This app is made for educational purposes only. Data it provides is not 100% accurate.</p>
     <p>Analyze stocks before investing.</p>
 </div>
 """
-st.markdown(footer, unsafe_allow_html=True)
-
-import yfinance as yf
-import requests
-
-def get_news(stock):
-    try:
-        company_name = yf.Ticker(stock).info['longName']
-        search_query = f'{stock} OR {company_name}'
-        url = f'https://newsapi.org/v2/everything?q={search_query}&apiKey={NEWS_API_KEY}&pageSize=5'
-        response = requests.get(url)
-        response.raise_for_status()  # This will raise an HTTPError if the request returned an unsuccessful status code
-        news_data = response.json()
-        return news_data
-    except requests.exceptions.HTTPError as http_err:
-        print(f"HTTP error occurred: {http_err}")
-        st.error("Failed to fetch stock information. Please check the ticker symbol and try again.")
-        return None
-    except Exception as err:
-        print(f"Other error occurred: {err}")
-        st.error("An unexpected error occurred. Please try again later.")
-        return None
+st.markdown(footer
