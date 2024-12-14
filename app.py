df0ebd254e67ae23e23e3ef924dfd0f852151ec3
EmilJohn434 <emilabraham434@gmail.com>
2024-12-14 05:36:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -182,8 +182,9 @@ elif choice == "Predict Silver Prices":
     changepoint_prior_scale = st.slider('Flexibility of Trend', 0.1, 10.0, 0.5, 0.1, format="%.1f")
 
     predict_prices(silver_data, 'Silver', n_years, smoothing_factor, changepoint_prior_scale,
-                   y_axis_values=[0, 10, 20, 30, 40, 50], y_axis
-                       y_axis_range=[0, 50])
+                   y_axis_values=[0, 10, 20, 30, 40, 50],
+                y_axis_range=[0, 50]
+
 
 elif choice == "Predict Crude Oil Prices":
     crude_data = load_data('CL=F')  # Use the correct ticker for Crude Oil Futures
